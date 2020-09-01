386f37f38c53cea62b146974c8308f5c3c73b047
pnayak1501 <pnayak1501@gmail.com>
2020-09-01 23:22:07 +0530

    Project12

---
+++ 12QuickChanger/app/src/main/java/in/prestige/quickchanger/MainActivity.java
@@ -0,0 +1,37 @@
+package in.prestige.quickchanger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+    RelativeLayout layout;
+    RadioGroup radioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.layout);
+        radioGroup= findViewById(R.id.rgroup);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case R.id.rbtnblue:layout.setBackgroundColor(Color.parseColor("#74B9FF"));
+                    break;
+                    case R.id.rbtngreen:layout.setBackgroundColor(Color.parseColor("#75DA8B"));
+                    break;
+                    case R.id.rbtnred:layout.setBackgroundColor(Color.parseColor("#E44236"));
+                    break;
+                }
+            }
+        });
+    }
+}
